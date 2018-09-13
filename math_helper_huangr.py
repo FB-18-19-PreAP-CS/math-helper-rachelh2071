450802003711a978395c33cfb9d34e4e81245af1
rachelh2071 <rachelh2071@gmail.com>
2018-09-13 14:11:07 -0500

    fix pythag theo

--- math_helper_huangr.py
+++ math_helper_huangr.py
@@ -1,12 +1,21 @@
 from math import *
+#import doctest
+#doctest.testmod()
 
-choice = input("Which formula would you like to use?")
+#choice = float(input("Which formula would you like to use?"))
 
 def pythagtheo(a,b,c):
-    '''when given 2 legs of a right triangle, returns length of hypotenuse'''
+    '''when given 2 sides of a right triangle, returns missing side
+    '''
     if a == 0:
-        return c**2-b**2
+        return math.sqrt(c**2-b**2)
     elif b == 0:
-        return c**2-a**2
+        return math.sqrt(c**2-a**2)
     else:
-        return a**2+b**2
+        return math.sqrt(a**2+b**2)
+    
+def herons(a,b,c):
+    '''when given 3 sides of a triangle, returns area using semiperimeter
+    '''
+    semi = (a + b + c)/2
+    return math.sqrt(semi(semi-a)(semi-b)(semi-c))

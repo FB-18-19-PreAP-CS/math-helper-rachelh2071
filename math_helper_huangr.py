b74aa28175eecb630823b5257801ad6221ad0771
rachelh2071 <rachelh2071@gmail.com>
2018-09-13 13:37:00 -0500

    basic pythag theo done

--- math_helper_huangr.py
+++ math_helper_huangr.py
@@ -1,4 +1,12 @@
 from math import *
 
-def sine(opp, hyp):
-    return opp/hyp
+choice = input("Which formula would you like to use?")
+
+def pythagtheo(a,b,c):
+    '''when given 2 legs of a right triangle, returns length of hypotenuse'''
+    if a == 0:
+        return c**2-b**2
+    elif b == 0:
+        return c**2-a**2
+    else:
+        return a**2+b**2

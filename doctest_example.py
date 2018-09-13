5ceab9b85f4012cd86ab2db64e40f81c96faca10
rachelh2071 <rachelh2071@gmail.com>
2018-09-13 13:58:04 -0500

    main function works

--- doctest_example.py
+++ doctest_example.py
@@ -54,10 +54,22 @@ def fib(n):
     return int((PHI**n - (-1/PHI)**n)/math.sqrt(5))
     
     
-    # change the return statement to the line below
-    # in order to resolve the errors
-    # return int((PHI**n - (-1/PHI)**n)/math.sqrt(5))
+def main():
+    while True:
+        num = float(input("Enter a positive integer:"))
+    
+        try:
+            print(f"The {int(num)}th fibonacci is {fib(num)}.")
+        except Exception as e:
+            print(f"Error: {e}")
+            break
+        
+    
+    
+    
+    
                 
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+    #import doctest
+    #doctest.testmod()
+    main()
